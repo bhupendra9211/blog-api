db7369cef51a8f05167d18d360b0bf2727ce0547
bhupendra9211 <shahbhupendra9211@gmail.com>
2025-01-10 12:44:32 +0545

    implemented users login and registration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UsersController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -10,3 +11,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',function(){
     return "api integrated";
 });
+Route::post('users/register',[UsersController::class,'userRegistration']);
+Route::post('users/login',[UsersController::class,'userLogin']);
